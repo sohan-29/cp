59d0892df7f9adfebc23325a8c549454c0f26a3f
Sohan <sohan291106@gmail.com>
2026-07-13 21:28:45 +0530

    Refactor and add counting functionality
    Refactored the code to remove the merging logic and added a new functionality to count how many times 'b' can be added to itself until it exceeds 'a'.

--- divide_using_binary_search.py
+++ divide_using_binary_search.py
@@ -1,25 +1,8 @@
-# Enter your code here. Read input from STDIN. Print output to STDOUT
-n,m=map(int,input().split())
-n1=list(map(int,input().split()))
-m1=list(map(int,input().split()))
-i=0
-j=0
-res=[]
-while i<n and j<m:
-    if n1[i]<=m1[j]:
-        res.append(n1[i])
-        i+=1
-    else:
-        res.append(m1[j])
-        j+=1
-while i<n:
-    res.append(n1[i])
-    i+=1
-while j<m:
-    res.append(m1[j])
-    j+=1
-mid=len(res)//2
-if len(res) % 2 == 0:
-    print(float((res[mid] + res[mid-1]) / 2))
-else:
-    print(float(res[mid]))
+a=int(input())
+b=int(input())
+q=0
+c=b
+while c<=a:
+    q+=1
+    c+=b
+print(q)
